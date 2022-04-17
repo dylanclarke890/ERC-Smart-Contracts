be978a55fcde2df8daa7d2c5658d54863cd1aa4e
"Dylan Clarke" <"dylanclarke890@gmail.com">
2022-04-17 16:31:07 +0100

    updated ERC 721 usage example

--- Usage/PotterToken.sol
+++ Usage/PotterToken.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.4;
 
 import "../ERC/ERC-721/ERC721.sol";
-import "../ERC/ERC-721/extensions/ERC721Burnable.sol";
 import "../ERC/ERC-721/extensions/ERC721URIStorage.sol";
+import "../ERC/ERC-721/extensions/ERC721Burnable.sol";
 import "../ERC/security/Pausable.sol";
 import "../ERC/access/Ownable.sol";
 import "../ERC/utils/Counters.sol";
 
-contract PotterToken is ERC721, Pausable, Ownable, ERC721Burnable {
+contract PotterToken is ERC721, ERC721URIStorage, Pausable, Ownable, ERC721Burnable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
@@ -16,7 +16,7 @@ contract PotterToken is ERC721, Pausable, Ownable, ERC721Burnable {
     constructor() ERC721("PotterToken", "PT") {}
 
     function _baseURI() internal pure override returns (string memory) {
-        return "://ipfs/SOMECID/";
+        return "ifps://";
     }
 
     function pause() public onlyOwner {
@@ -27,10 +27,11 @@ contract PotterToken is ERC721, Pausable, Ownable, ERC721Burnable {
         _unpause();
     }
 
-    function safeMint(address to) public onlyOwner {
+    function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
@@ -39,5 +40,20 @@ contract PotterToken is ERC721, Pausable, Ownable, ERC721Burnable {
         override
     {
         super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
 }
