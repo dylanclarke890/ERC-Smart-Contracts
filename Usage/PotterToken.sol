c3a504986e76e6ebc46775f55a0e5acac085f779
"Dylan Clarke" <"dylanclarke890@gmail.com">
2022-04-17 16:22:18 +0100

    added sample usage of ERC721

---
+++ Usage/PotterToken.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "../ERC/ERC-721/ERC721.sol";
+import "../ERC/ERC-721/extensions/ERC721Burnable.sol";
+import "../ERC/security/Pausable.sol";
+import "../ERC/access/Ownable.sol";
+import "../ERC/utils/Counters.sol";
+
+contract PotterToken is ERC721, Pausable, Ownable, ERC721Burnable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("PotterToken", "PT") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "://ipfs/SOMECID/";
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+}
